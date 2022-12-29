883f157a7357f70845106dce79dc74e0f9ffebf1
Maicol Hernandez Peralta <maicolhernandez420@gmail.com>
2022-12-29 17:57:09 -0500

    table CreatePruductsTable

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -7,4 +7,13 @@ use Illuminate\Http\Request;
 class MainController extends Controller
 {
     //
+
+    public function index(Request $request)
+    {
+        // $name = config('app.undefined', 'welcome');
+        // dd($name);
+        // dump($name);
+
+        return view('welcome');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-})->name('main');
+Route::get('/', 'Maincontroller@index')->name('main');
 
 
 /**
